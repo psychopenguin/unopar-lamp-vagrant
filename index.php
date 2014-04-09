b675d8198e0b1ba6872a8253d377724d722ef5f7
Fabio Brito d'Araujo e Oliveira <fbaoliveira@sp.r7.com>
2014-04-09 15:18:02 -0300

    index.php with phpinfo

---
+++ index.php
@@ -0,0 +1,16 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>Teste Bootstrap</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<h1>Teste Bootstrap</h1>
+</body>
+</html>
+
